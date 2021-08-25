45fde28dd19c270f45cf1f3fd3b64bd0611e2022
Alyona Gnevusheva <tchernovaalena@gmail.com>
2021-08-25 17:12:33 +0300

    refactor: applied composition

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CsvFileReader = void 0;
 var fs_1 = __importDefault(require("fs"));
-// using generic for the type of the data inside of the reader, making class abstract
 var CsvFileReader = /** @class */ (function () {
     function CsvFileReader(filename) {
         this.filename = filename;
@@ -18,36 +17,8 @@ var CsvFileReader = /** @class */ (function () {
         }).split('/n')
             .map(function (row) {
             return row.split(',');
-        })
-            .map(this.mapRow);
+        });
     };
     return CsvFileReader;
 }());
 exports.CsvFileReader = CsvFileReader;
-/* export class CsvFileReader {
-    // data is an array of tuples(Match Data)
-    data: MatchData[] = [];
-    
-    constructor(public filename: string) {}
-
-    read(): void {
-        this.data = fs.readFileSync(this.filename, {
-            encoding: 'utf-8'
-        }).split('/n')
-        .map((row: string): string[] => {
-            return row.split(',');
-        })
-        .map((row: string[]): MatchData => {
-            return [
-                dateStringToDate(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                // Type Assertion - override TS typing, saying that it may have only MatchResult values
-                row[5] as MatchResult,
-                row[6],
-            ]
-        });
-    }
-} */ 
--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,30 +1,9 @@
 import fs from 'fs';
 
-// using generic for the type of the data inside of the reader, making class abstract
-export abstract class CsvFileReader<T> {
+
+export class CsvFileReader {
     // data is an array of tuples(Match Data)
-    data: T[] = [];
-    
-    constructor(public filename: string) {}
-
-    // saying that mapRow method will be defined in children's classes
-    abstract mapRow(row: string[]): T;
-
-    read(): void {
-        this.data = fs.readFileSync(this.filename, {
-            encoding: 'utf-8'
-        }).split('/n')
-        .map((row: string): string[] => {
-            return row.split(',');
-        })
-        .map(this.mapRow);
-    }  
-}
-
-
-/* export class CsvFileReader {
-    // data is an array of tuples(Match Data)
-    data: MatchData[] = [];
+    data: string[][] = [];
     
     constructor(public filename: string) {}
 
@@ -34,18 +13,6 @@ export abstract class CsvFileReader<T> {
         }).split('/n')
         .map((row: string): string[] => {
             return row.split(',');
-        })
-        .map((row: string[]): MatchData => {
-            return [
-                dateStringToDate(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                // Type Assertion - override TS typing, saying that it may have only MatchResult values
-                row[5] as MatchResult,
-                row[6],
-            ]
         });
     }
-} */
+}
